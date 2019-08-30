7a791dde0034389eeb40f5d3cac3418a819c921c
mpilakowski <54590507+mpilakowski@users.noreply.github.com>
2019-08-30 15:09:59 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Maverick
+ * Date: 2019/27/19
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Maverick\n");
+
+  return 0;
+}
